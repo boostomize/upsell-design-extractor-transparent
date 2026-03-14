919936221fd83e8bf7d6f366f2af036b426e45fa
boostomize <244904665+boostomize@users.noreply.github.com>
2026-03-14 16:08:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
 const SHOPIFY_FETCH_HEADERS = {
   "User-Agent":
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36",
